fec57e842e28c51db45dd2b00c6b81ff70251e4c
Dhiaeddine Gharsallah <dhiaeddine.gharsallah@gmail.com>
2020-03-10 20:20:27 +0100

    Create Inorder traversal - Medium.cpp

---
+++ Inorder traversal - Medium.cpp
@@ -0,0 +1,49 @@
+// *
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  * };
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+      if (!root) {
+          return vector<int>();
+      }
+      vector<int> ans;
+      stack<TreeNode*> s;
+      s.push(root);
+      while(!s.empty()) {
+          TreeNode* cur = s.top();
+          if (cur->left == NULL) {
+              ans.push_back(cur->val);
+              TreeNode* tmp = cur->right;
+              s.pop();
+              if (tmp != NULL) {
+                  s.push(tmp);
+              }
+          } else {
+              s.push(cur->left);
+              cur->left = NULL;
+          }
+      }
+      return ans;
+       //  if (!root) {
+       //      return vector<int>();
+       //  }
+       // vector<int> ans;
+       // if (root->left != NULL) {
+       //     vector<int> l = inorderTraversal(root->left);
+       //     ans.insert(ans.end(), l.begin(), l.end());
+       // }
+       //  ans.push_back(root->val);
+       //  if (root->right != NULL) {
+       //      vector<int> r = inorderTraversal(root->right);
+       //      ans.insert(ans.end(), r.begin(), r.end());
+       //  }
+       //  return ans;
+    }
+};

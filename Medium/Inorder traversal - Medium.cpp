90ef16d4b0161538962157d074da15bc2aee5609
Dhiaeddine Gharsallah <dhiaeddine.gharsallah@gmail.com>
2020-03-12 19:53:15 +0100

    Update Inorder traversal - Medium.cpp

--- Medium/Inorder traversal - Medium.cpp
+++ Medium/Inorder traversal - Medium.cpp
@@ -9,6 +9,26 @@
 
 class Solution {
 public:
+    
+    // Recursive solution
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (!root) {
+            return vector<int>();
+        }
+       vector<int> ans;
+       if (root->left != NULL) {
+           vector<int> l = inorderTraversal(root->left);
+           ans.insert(ans.end(), l.begin(), l.end());
+       }
+        ans.push_back(root->val);
+        if (root->right != NULL) {
+            vector<int> r = inorderTraversal(root->right);
+            ans.insert(ans.end(), r.begin(), r.end());
+        }
+        return ans;
+    }
+    
+    // Iterative solution
     vector<int> inorderTraversal(TreeNode* root) {
       if (!root) {
           return vector<int>();
@@ -31,19 +51,5 @@ public:
           }
       }
       return ans;
-       //  if (!root) {
-       //      return vector<int>();
-       //  }
-       // vector<int> ans;
-       // if (root->left != NULL) {
-       //     vector<int> l = inorderTraversal(root->left);
-       //     ans.insert(ans.end(), l.begin(), l.end());
-       // }
-       //  ans.push_back(root->val);
-       //  if (root->right != NULL) {
-       //      vector<int> r = inorderTraversal(root->right);
-       //      ans.insert(ans.end(), r.begin(), r.end());
-       //  }
-       //  return ans;
     }
 };

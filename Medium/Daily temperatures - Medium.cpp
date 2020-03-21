2b683044fc9169fa1a24a60880105ffda4109f82
Dhiaeddine Gharsallah <dhiaeddine.gharsallah@gmail.com>
2020-03-21 01:28:40 +0100

    Update Daily temperatures - Medium.cpp

--- Medium/Daily temperatures - Medium.cpp
+++ Medium/Daily temperatures - Medium.cpp
@@ -19,4 +19,20 @@ public:
         }
         return ans;
     }
+    /* 
+    // A faster solution using stack
+    vector<int> dailyTemperatures(vector<int>& T) {
+        int n = T.size();
+        stack<int> s;
+        vector<int> res(n, 0);
+        for (int i = 0; i < n; ++i) {
+            while(!s.empty() && T[s.top()] < T[i]) {
+                res[s.top()] = i - s.top();
+                s.pop();
+            }
+            s.push(i);
+        }
+        return res;
+    }
+    */
 };

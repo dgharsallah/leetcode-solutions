6f4ffada3a11de9d806aed89df9a086c13b42e9d
Dhiaeddine Gharsallah <dhiaeddine.gharsallah@gmail.com>
2020-04-01 21:34:37 +0100

    Update Find duplicate number - Medium.py

--- Medium/Find duplicate number - Medium.py
+++ Medium/Find duplicate number - Medium.py
@@ -1,4 +1,5 @@
 class Solution:
+    # Binary search the answer
     def findDuplicate(self, nums: List[int]) -> int:
         l, h, m = 0, len(nums) - 1, len(nums) >> 1
         while l <= h:
@@ -8,5 +9,18 @@ class Solution:
                 l = m + 1
             m = (h + l) >> 1
         return m + 1
-        
+    # Floyd's cycle detection algorithm
+    def findDuplicate(self, nums: List[int]) -> int:
+        hare = nums[0]
+        tortoise = nums[0]
+        while True:
+            hare = nums[nums[hare]]
+            tortoise = nums[tortoise]
+            if nums[hare] == nums[tortoise]:
+                break
+        hare = nums[0]
+        while hare != tortoise:
+            hare = nums[hare]
+            tortoise = nums[tortoise]
+        return tortoise
         

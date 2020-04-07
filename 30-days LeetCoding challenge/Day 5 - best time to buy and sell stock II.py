6f6cb5af9d3c9d0770a9789208b34bdad1ec4b91
Dhiaeddine Gharsallah <dhiaeddine.gharsallah@gmail.com>
2020-04-07 12:11:50 +0100

    Update Day 5 - best time to buy and sell stock II.py

--- 30-days LeetCoding challenge/Day 5 - best time to buy and sell stock II.py
+++ 30-days LeetCoding challenge/Day 5 - best time to buy and sell stock II.py
@@ -1,7 +1,23 @@
 class Solution:
+    
     def maxProfit(self, prices: List[int]) -> int:
         maxProfit = 0
         for i in range(len(prices)):
             if i > 0 and prices[i] - prices[i - 1] > 0:
                 maxProfit += prices[i] - prices[i - 1]
+        return maxProfit
+    
+    # peak valley approach
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        valley = 0
+        i = 0
+        while i < len(prices) - 1:
+            while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
+                i += 1
+            valley = prices[i]
+            while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
+                i += 1
+            peak = prices[i]
+            maxProfit += peak - valley
         return maxProfit

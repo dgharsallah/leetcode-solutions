c9e0401000ee7cc467c0f6852ed1b8fde55aff7d
Dhiaeddine Gharsallah <dhiaeddine.gharsallah@gmail.com>
2020-04-18 19:05:44 +0100

    Create Day 18 - Minimum path sum.py

---
+++ 30-days LeetCoding challenge/Day 18 - Minimum path sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0]) if len(grid) > 0 else 0
+        if n == 1 and m == 1:
+            return grid[0][0]
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                if i == 0 and j == 0:
+                    continue
+                up = 1e9
+                left = 1e9
+                if i > 0:
+                    up = grid[i - 1][j]
+                if j > 0:
+                    left = grid[i][j - 1]
+                grid[i][j] += min(up, left)
+                ans = grid[i][j]
+        return ans
